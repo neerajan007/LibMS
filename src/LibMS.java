d43463ff32fe0576a5dc329428824a53f6ceb816
nirajan <neerajanthapaliya01@gmail.com>
2023-05-17 19:15:48 +0545

    erh

--- src/LibMS.java
+++ src/LibMS.java
@@ -1,5 +1,23 @@
+class Library{
+        String [] availableBooks = new String[100];
+        String [] issuedBooks = new String[100];
+        int numAvailableBooks = 0;
+        int numIssuedBooks = 0;
+
+        //adding books
+        void addBook(String book) {
+            this.availableBooks[this.numAvailableBooks] = book;
+            this.numAvailableBooks++;
+            System.out.printf("%s has been added", book);
+        }
+
+        void
+
+    }
+
+}
+
 public class LibMS  {
-    sdg
-    dfg
+
 }
 
